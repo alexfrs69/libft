3ae11341b1110c1488c25e371c579c8e34959b22
Alexandre FRANCOIS <afrancoi@e2r3p21.42.fr>
2018-11-07 18:17:34 +0100

    libft.h

---
+++ includes/libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afrancoi <afrancoi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/07 18:13:42 by afrancoi          #+#    #+#             */
+/*   Updated: 2018/11/07 18:16:34 by afrancoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+/* Includes */
+
+# include <unistd.h>
+
+/* Prototype */
+
+void	ft_putchar(char c);
+int		ft_strlen(char *str);
+
+#endif
