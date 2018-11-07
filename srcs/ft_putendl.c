800da7654cd2ffadca85b1939a8e407e84c4a32a
Alexandre FRANCOIS <afrancoi@e2r3p21.42.fr>
2018-11-07 18:38:30 +0100

    ft_putendl.c & proto

--- srcs/ft_putendl.c
+++ srcs/ft_putendl.c
@@ -1,29 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_putendl.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: afrancoi <afrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/07 18:13:42 by afrancoi          #+#    #+#             */
-/*   Updated: 2018/11/07 18:38:07 by afrancoi         ###   ########.fr       */
+/*   Created: 2018/11/07 18:35:47 by afrancoi          #+#    #+#             */
+/*   Updated: 2018/11/07 18:37:18 by afrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-/* Includes */
-
-# include <unistd.h>
-
-/* Prototype */
-
-void	ft_putchar(char c);
-void	ft_putchar_fd(char c, int fd);
-int		ft_strlen(char *str);
-void	ft_putstr(char const *str);
-void	ft_putstr_fd(char const *str, int fd);
-void	ft_putendl(char const *s);
-
-#endif
+void	ft_putendl(char const *s)
+{
+	ft_putstr(s);
+	ft_putchar('\n');
+}
