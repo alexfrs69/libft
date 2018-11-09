8cf0e156d8c93157b6c39e801f6926fc06b80dc9
Alexandre FRANCOIS <afrancoi@e2r3p19.42.fr>
2018-11-09 14:17:04 +0100

    strcmp fix

--- srcs/ft_strcmp.c
+++ srcs/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: afrancoi <afrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/08 20:15:08 by afrancoi          #+#    #+#             */
-/*   Updated: 2018/11/08 20:16:34 by afrancoi         ###   ########.fr       */
+/*   Updated: 2018/11/09 14:10:44 by afrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ int		ft_strcmp(const char *s1, const char *s2)
 		s1++;
 		s2++;
 	}
-	return (*s1 - *s2);
+	return (*(const unsigned char*)s1 - *(const unsigned char*)s2);
 }
