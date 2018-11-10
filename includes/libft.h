4481a8bc090df1ddf049c4d5fd83d16b192303e2
Alexandre FRANCOIS <afrancoi@e2r3p19.42.fr>
2018-11-10 22:34:13 +0100

    libft arrengments

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: afrancoi <afrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 18:13:42 by afrancoi          #+#    #+#             */
-/*   Updated: 2018/11/10 22:24:26 by afrancoi         ###   ########.fr       */
+/*   Updated: 2018/11/10 22:33:46 by afrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,21 @@
 # include <string.h>
 # include <stdlib.h>
 
-/* Prototype */
+/* Prototypes */
 
+/* Put fucnctions */
 void	ft_putchar(char c);
 void	ft_putchar_fd(char c, int fd);
+void	ft_putstr(char const *str);
+void	ft_putstr_fd(char const *str, int fd);
+void	ft_putendl(char const *s);
+void	ft_putendl_fd(char const *s, int fd);
+void	ft_putnbr(int nb);
+void	ft_putnbr_fd(int nb, int fd);
 
+/* String functions */
 size_t	ft_strlen(const char *str);
+
 int		ft_strcmp(char const *s1, const char *s2);
 int		ft_strncmp(char const *s1, const char *s2, size_t n);
 char	*ft_strdup(const char *s1);
@@ -33,24 +42,17 @@ char	*ft_strncpy(char *dst, const char *src, size_t len);
 char	*ft_strcat(char *s1, const char *s2);
 char	*ft_strncat(char *s1, const char *s2, size_t n);
 
-void	ft_putstr(char const *str);
-void	ft_putstr_fd(char const *str, int fd);
-
-void	ft_putendl(char const *s);
-void	ft_putendl_fd(char const *s, int fd);
-
-void	ft_putnbr(int nb);
-void	ft_putnbr_fd(int nb, int fd);
-
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 
+/* Is functions */
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 
+/* Mem functions */
 void	*ft_memset(void *b, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
