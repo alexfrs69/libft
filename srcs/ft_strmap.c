05f210423679a5ca84aac4d8b276f0fa1c2dd3cf
Alexandre FRANCOIS <afrancoi@student.42.fr>
2018-11-12 19:43:51 +0100

    ft_strmap.c

--- srcs/ft_strmap.c
+++ srcs/ft_strmap.c
@@ -6,11 +6,27 @@
 /*   By: afrancoi <afrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 18:52:07 by afrancoi          #+#    #+#             */
-/*   Updated: 2018/11/12 18:52:37 by afrancoi         ###   ########.fr       */
+/*   Updated: 2018/11/12 19:43:38 by afrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strmap(char const *s, char (*f)(char))
 {
+	char	*ret;
+	size_t	i;
 
+	if (!s)
+		return (NULL);
+	if (!(ret = (char*)malloc(sizeof(char) * ft_strlen(s) + 1)))
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		ret[i] = f(s[i]);
+		i++;
+	}
+	ret[i] = '\0';
+	return (ret);
 }
