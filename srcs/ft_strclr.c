5bd3ee873a98b9bc884cb209d1fbefedf4fcc6c2
Alexandre FRANCOIS <afrancoi@student.42.fr>
2018-11-13 19:42:44 +0100

    fix ft_strclr was calling memset instead of ft_memset

--- srcs/ft_strclr.c
+++ srcs/ft_strclr.c
@@ -6,7 +6,7 @@
 /*   By: afrancoi <afrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 18:42:42 by afrancoi          #+#    #+#             */
-/*   Updated: 2018/11/12 18:44:17 by afrancoi         ###   ########.fr       */
+/*   Updated: 2018/11/13 19:42:25 by afrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ void	ft_strclr(char *s)
 {
 	if (!s)
 		return ;
-	memset(s, '\0', ft_strlen(s));
+	ft_memset(s, '\0', ft_strlen(s));
 }
