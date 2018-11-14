2baf322b1c132995ca1c82fdf9d43990d51ad1e3
Alexandre FRANCOIS <afrancoi@student.42.fr>
2018-11-14 18:43:11 +0100

    ft_lstmap

--- srcs/ft_lstmap.c
+++ srcs/ft_lstmap.c
@@ -6,30 +6,22 @@
 /*   By: afrancoi <afrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/14 05:36:42 by afrancoi          #+#    #+#             */
-/*   Updated: 2018/11/14 05:51:24 by afrancoi         ###   ########.fr       */
+/*   Updated: 2018/11/14 18:41:30 by afrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-static	void	ft_del(void *content, size_t content_size)
-{
-
-}
-
 t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list *ret;
-	t_list *tmp;
 
-	if (!lst)
-		return (NULL);
-	while (lst)
+	if (lst)
 	{
-		tmp = ft_lstnew(lst->content, lst->content_size);
-		ret = ft_lstadd(&ret, tmp);
-		ft_lstdelone(&tmp,)
-		lst = lst->next;
+		ret = f(lst);
+		ret->next = ft_lstmap(lst->next, f);
+		return (ret);
 	}
+	return (NULL);
 }
